e698744a611af14fd7cb1be3e7b0cb808a1b3a83
vitorfgr <62887659+vitorfgr@users.noreply.github.com>
2023-10-19 16:51:29 -0300

    aula2

--- python_odoo_github.py
+++ python_odoo_github.py
@@ -1,7 +1,11 @@
 import xmlrpc.client
 import sys
+import mysql.connector
 
-#Connection
+
+
+
+#Connection ODOO
 url = "https://edu-smarters-navy.odoo.com"
 db = 'edu-smarters-navy'
 username = 'gustavorp3@al.insper.edu.br'
@@ -19,63 +23,70 @@ if uid == False:
 models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
 
 
+# Operaçoes
+def WO_Start(WO_id):
+    model_name = 'mrp.workorder'
+    WO_start = models.execute_kw(db,uid,password, model_name,'button_start',[WO_id])
+    return WO_start
+
+def WO_WriteProduction(WO_id, qty_produced):
+    model_name = 'mrp.workorder'
+    wo_write = models.execute_kw(db, uid, password, model_name, 'write', [[WO_id], {'qty_produced': qty_produced}] )
+    wo_write = models.execute_kw(db, uid, password, model_name, 'write', [[WO_id], {'qty_producing': qty_produced}] )
+    return wo_write
+
+def WO_Done(WO_id):
+    model_name = 'mrp.workorder'
+    WO_done = models.execute_kw(db,uid,password, model_name,'button_done',[WO_id])
+    return WO_done
+
+def MO_MarkAsDone(MO_id):
+    model_name = 'mrp.production'
+    MO_done = models.execute_kw(db, uid, password, model_name, 'button_mark_done', [[MO_id]] )
+    return MO_done
+
+# Dicionario 
+maquina = {'Torneamento':'TR10', 'Montagem':'MT10', 'Teste':'ET10', 'Usinagem':'CU10'}
+
+
 running = True
 while running:
 
-    # Search MO - Odoo
-    # *********
+# Search MO - Odoo
+# *********
     model_name = 'mrp.production'
     domain = [[ ['state', '=', 'confirmed'] ]]
     parameters = {'fields': ['name', 'product_id', 'product_qty', 'state', 'components_availability', 'workorder_ids']}
     records = models.execute_kw(db, uid, password, model_name, 'search_read', domain, parameters)
-    print("\nManuf Orders:")
-    for key in records:
-        print(key)
+    
+
+    if not records:
+        pass
+
+    else:
+        running = False
+        key = records[0]
+        model_name = 'mrp.workorder'
+        domain = [key['id']]
+        parameters = {'fields':
+                      ['name','workcenter_id','qty_production','qty_producing','qty_produced','working_state','production_state','state','is_produced']}
+        records = models.execute_kw(db, uid, password, model_name, 'read', domain, parameters)
+        WO_key = records[0]
 
 
-    # Search WO - Odoo
-    # ********
-    model_name = 'mrp.workorder'
-    domain = ['workorder_ids']
-    parameters = {'fields':
-                  ['name','workcenter_id','qty_production','qty_producing','qty_produced','working_state','production_state','state','is_produced']}
-    records = models.execute_kw(db, uid, password, model_name, 'read', domain, parameters)
-    print("\nWork Orders:")
-    for key in records:
-        print(key)
+if WO_key and WO_Status == 0:
+    WO_Start(WO_key[id])
 
-    if records != 0:
-        # START WO
+
+if WO_Status == 1:
+    WO_WriteProduction(WO_key['id'], qty_produced)
+
+
+
+
+
         
 
 
-# START WO
-#*********
-#"""WO_id = 22
-#model_name = 'mrp.workorder'
-#parameters = {}
-#WO_start = models.execute_kw(db, uid, password, model_name, 'button_start', [WO_id], parameters)
-#print("Bt WO start:", WO_start)
-#
-## WRITE WO (QTY PRODUCED)
-## ***********************
-#WO_id = 22
-#model_name = 'mrp.workorder'
-#WO_write = models.execute_kw(db, uid, password, model_name, 'write', [[WO_id], {'qty_produced': 10}] )
-#print("Write WO production:", WO_write)
-#
-## STOP WO
-##********
-#WO_id = 22
-#model_name = 'mrp.workorder'
-#parameters = {}
-#WO_stop = models.execute_kw(db, uid, password, model_name, 'button_done', [WO_id], parameters)
-#print("Bt WO done:", WO_stop)
-#
-## MASK MO AS DONE
-##****************
-#MO_id = 22
-#model_name = 'mrp.production'
-#parameters = {}
-#MO_done = models.execute_kw(db, uid, password, model_name, 'button_mark_done', [MO_id], parameters)
-#print("Bt MO Mark as Done:", MO_done)
+
+
